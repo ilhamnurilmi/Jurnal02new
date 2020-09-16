a704f5191cafe52950e3fd878b74304e040bbb7b
ilhamnurilmi <ilhamnurilmi@student.telkomuniversity.ac.id>
2020-09-16 11:17:52 +0700

    Jurnal01new.java

---
+++ Jurnal01new.java
@@ -0,0 +1,8 @@
+public class Jurnal01new {
+
+    public static void main(String[] args){
+	    System.out.println("Nama  : ILHAM NUR ILMI");
+		System.out.println("Kelas : D3IF 44-04");
+		System.out.println("NIM   : 6706204115");
+	}
+}
